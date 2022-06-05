b02107cf5927aeb4e3868d993ccc14cfc0ba0640
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2022-06-05 16:14:36 +0200

    add cidr_block

--- terraform/0-modules/vpc/main.tf
+++ terraform/0-modules/vpc/main.tf
@@ -2,7 +2,7 @@
 #               VPC
 #-----------------------------------------------
 resource "aws_vpc" "this" {
-  cidr_block           = var.vpc_cidr_block[lower(var.environment)]
+  cidr_block           = var.cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
 
